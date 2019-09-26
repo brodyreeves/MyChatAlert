4113e2a4ea9dd6fe95429b0cc4b73b3ed79e3b4d
Brody Reeves <brodyreeves@gmail.com>
2019-09-26 10:22:33 -0700

    Filter messages from player
    See #7

--- core.lua
+++ core.lua
@@ -1,7 +1,7 @@
 MyChatAlert = LibStub("AceAddon-3.0"):NewAddon("MyChatAlert", "AceConsole-3.0", "AceEvent-3.0")
 
 local AceGUI = LibStub("AceGUI-3.0")
-local colorG, colorR, colorY, colorW = "|cFF00FF00", "|cffff0000", "|cFFFFFF00", "|r" -- text color flags
+local C_G, C_R, C_Y, C_W = "|cFF00FF00", "|cffff0000", "|cFFFFFF00", "|r" -- text color flags
 
 function MyChatAlert:OnInitialize()
     -- Called when the addon is loaded
@@ -21,13 +21,15 @@ end
 
 -- Event Handlers
 function MyChatAlert:CHAT_MSG_CHANNEL(event, message, author, _, channel)
+    if author == UnitName("player") then return end -- don't do anything if it's your own message
+
     for k, ch in pairs(self.db.profile.channels) do
         if event == "CHAT_MSG_CHANNEL" and channel:lower() == ch:lower() then
             for k2, word in pairs(self.db.profile.words) do
                 if message:lower():find(word:lower()) then -- Alert message
                     if self.db.profile.soundOn then PlaySound(self.db.profile.sound) end
                     if self.db.profile.printOn then
-                        LibStub("AceConsole-3.0"):Print(colorG .. "Keyword <" .. colorY .. word .. colorG .. "> seen from " .. colorY .. "[" .. author .. "]" .. colorG .. ": " .. colorY .. message)
+                        LibStub("AceConsole-3.0"):Print(C_G .. "Keyword <" .. C_Y .. word .. C_G .. "> seen from " .. C_Y .. "[" .. author .. "]" .. C_G .. ": " .. C_Y .. message)
                     end
                     self:AddAlert(word, author, message)
                     break -- matched the message so stop looping
